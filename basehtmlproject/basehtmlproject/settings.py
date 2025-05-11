e95a692d7ea6bdbe022f875c018b60191410ce61
DeniCoder <denis2042@yandex.ru>
2025-05-11 19:54:49 +0300

    change code

--- basehtmlproject/basehtmlproject/settings.py
+++ basehtmlproject/basehtmlproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'basehtmlproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
